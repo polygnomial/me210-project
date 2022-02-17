cd307c3020373cee4fc5439c6f2188648997a492
Paxton Scott <paxtonsc@stanford.edu>
2022-02-17 11:17:26 -0800

    starting to implement pwm for motors

--- Kabob_Code/src/main.cpp
+++ Kabob_Code/src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include <AccelStepper.h>
+
+#define PWM_FREQUENCY               490 // hz
+#define MILLISECONDS(time)({time * 1000;})
+#define MICROSECONDS(time)({time * 1000000;})
+#define PWM_CYCLE_LENGTH            MICROSECONDS((float)1/PWM_FREQUENCY)
 
 int outputPinF1 = 3; 
 int outputPinB1 = 4;  
@@ -6,12 +13,21 @@ int outputPinF2 = 11;
 int outputPinB2 = 12;
 int state = HIGH; //low is bwds, high is fwds
 
+
+IntervalTimer pin_timer;
+volatile unsigned int motor_running;
+volatile unsigned int pin_state;
+unsigned long previous_millis; 
+static double duty_cycle;
+static double off_time;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //Serial.println("Hello, world!");
+  Serial.println("Hello, world!");
+
   // Initialize pins
-  pinMode(inputPin, INPUT);
+  //pinMode(inputPin, INPUT);
   pinMode(outputPinF1,OUTPUT);
   pinMode(outputPinB1,OUTPUT);
   pinMode(outputPinF2,OUTPUT);
@@ -21,6 +37,14 @@ void setup() {
   analogWrite(outputPinB2, LOW);
   analogWrite(outputPinF1, LOW);
   analogWrite(outputPinF2, LOW);  
+
+
+  // handle PWM
+  duty_cycle = 0.5;
+  off_time = PWM_CYCLE_LENGTH - duty_cycle;
+  pin_timer.begin(TogglePin, duty_cycle);
+
+
 }
 
 void loop() {
