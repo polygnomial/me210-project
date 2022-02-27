e371c06e2c71b6ab1984304cd17af1cf130349ce
Flynn Dreilinger <flynnd@stanford.edu>
2022-02-27 12:52:04 -0800

    add comments and format

--- Kabob_Code/src/Actuators/Chassis.h
+++ Kabob_Code/src/Actuators/Chassis.h
@@ -5,45 +5,55 @@
 
 class Chassis
 {
-  public:
-    Chassis(unsigned rf_pin, 
-            unsigned rb_pin, 
-            unsigned lf_pin, 
-            unsigned lb_pin,
-            unsigned re_pin1, // polarity of these does not matter as far as I can tell
-            unsigned re_pin2,
-            unsigned le_pin1,
-            unsigned le_pin2, 
-            double hub_to_hub_distance,
-            double wheel_circumference) : 
-      right(rf_pin, rb_pin, re_pin1, re_pin2),
-      left(lf_pin, lb_pin, le_pin1, le_pin2),
-      chassis_circumference(hub_to_hub_distance*PI),
-      wheel_circumference(wheel_circumference),
-      ninety_degrees(chassis_circumference / 4 / wheel_circumference * 360)
-      {};
-    
-    void activity(void);
-    void stop(void);
-    void move_forward(double distance, uint8_t speed = 100);
-    void move_backward(double distance, uint8_t speed = 100);
-    void turn_right(uint8_t speed = 100);
-    void turn_left(uint8_t speed = 100);
-    void turn_cw(double angle, uint8_t speed = 100);
-    void turn_ccw(double angle, uint8_t speed = 100);
-    void move_forward_at_speed(uint8_t speed);
-    void move_backward_at_speed(uint8_t speed);
-    void turn_right_at_speed(uint8_t speed);
-    void turn_left_at_speed(uint8_t speed);
-  
+public:
+  Chassis(unsigned rf_pin,
+          unsigned rb_pin,
+          unsigned lf_pin,
+          unsigned lb_pin,
+          unsigned re_pin1,
+          unsigned re_pin2,
+          unsigned le_pin1,
+          unsigned le_pin2,
+          double hub_to_hub_distance,
+          double wheel_circumference) : right(rf_pin, rb_pin, re_pin1, re_pin2),
+                                        left(lf_pin, lb_pin, le_pin1, le_pin2),
+                                        chassis_circumference(hub_to_hub_distance * PI),
+                                        wheel_circumference(wheel_circumference),
+                                        ninety_degrees(chassis_circumference / 4 / wheel_circumference * 360){};
 
-    Motor right;
-    Motor left;
+  void activity(void);
+  void stop(void);
 
-    private:
-      double chassis_circumference,
-             wheel_circumference,
-             ninety_degrees;
+  /* forward and backward
+   * distance (centimeters)
+   * - if stop() is called, the Chassis will reset its target.
+   */
+  void move_forward(double distance, uint8_t speed = 100);
+  void move_backward(double distance, uint8_t speed = 100);
+
+  /* turn_right and turn_left
+   * rotate 90°
+   */
+  void turn_right(uint8_t speed = 100);
+  void turn_left(uint8_t speed = 100);
+
+  /* turn_cw and turn_ccw
+   * distance (centimeters)
+   * - if stop() is called, the Chassis will reset its target.
+   */
+  void turn_cw(double angle, uint8_t speed = 100);
+  void turn_ccw(double angle, uint8_t speed = 100);
+
+  void move_forward_at_speed(uint8_t speed);
+  void move_backward_at_speed(uint8_t speed);
+  void turn_right_at_speed(uint8_t speed);
+  void turn_left_at_speed(uint8_t speed);
+
+  Motor right;
+  Motor left;
+
+private:
+  double chassis_circumference,
+      wheel_circumference,
+      ninety_degrees;
 };
-
-//TODO decide when to reset the counter things
--- Kabob_Code/src/Actuators/Motor.h
+++ Kabob_Code/src/Actuators/Motor.h
@@ -7,45 +7,41 @@
 
 class Motor
 {
-  public:
-    Motor(unsigned f_pin, 
-          unsigned b_pin, 
-          unsigned e_pin1, // polarity of these does not matter as far as I can tell
-          unsigned e_pin2) :
-      f_pin(f_pin),
-      b_pin(b_pin),
-      encoder(e_pin1, e_pin2){
-        pinMode(f_pin, OUTPUT);
-        pinMode(b_pin, OUTPUT);
-      };
-    // can optionally add encoder to make fake inertial measured movements
+public:
+  Motor(unsigned f_pin,
+        unsigned b_pin,
+        unsigned e_pin1, // polarity of these does not matter as far as I can tell
+        unsigned e_pin2) : f_pin(f_pin),
+                           b_pin(b_pin),
+                           encoder(e_pin1, e_pin2)
+  {
+    pinMode(f_pin, OUTPUT);
+    pinMode(b_pin, OUTPUT);
+  };
 
-    
-    void activity(void);
+  void activity(void);
+  void stop(void);
 
-    void stop(void);
-    /* cw (clockwise) and ccw (counterclockwise):
-     * angle (degrees)
-     * - clockwise when looking at the motor along the shaft
-     * - if stop() is called, the Motor will reset its target
-     */
-    void cw(uint32_t angle, uint8_t speed);
-    void ccw(uint32_t angle, uint8_t speed);
-    
-    void cw_at_speed(uint8_t speed);
-    void ccw_at_speed(uint8_t speed);
-    
-    int get_pos(void);
+  /* cw (clockwise) and ccw (counterclockwise):
+   * angle (degrees)
+   * - clockwise when looking at the motor along the shaft
+   * - if stop() is called, the Motor will reset its target
+   */
+  void cw(uint32_t angle, uint8_t speed);
+  void ccw(uint32_t angle, uint8_t speed);
 
-  private:
-    unsigned f_pin,
-             b_pin;
+  void cw_at_speed(uint8_t speed);
+  void ccw_at_speed(uint8_t speed);
 
-    Encoder encoder;
+  int get_pos(void);
 
-    int pos;
-    double target;
-    double overflow;
+private:
+  unsigned f_pin,
+      b_pin;
+
+  Encoder encoder;
+
+  int pos;
+  double target;
+  double overflow;
 };
-
-//TODO decide when to reset the counter things
