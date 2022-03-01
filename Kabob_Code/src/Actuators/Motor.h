b807df4712247ba0175059464fe97aa06d772430
Flynn Dreilinger <flynnd@stanford.edu>
2022-03-01 00:43:55 -0800

    do more things

--- Kabob_Code/src/Actuators/Motor.h
+++ Kabob_Code/src/Actuators/Motor.h
@@ -2,20 +2,13 @@
 
 #include "Arduino.h"
 
-#define ENCODER_OPTIMIZE_INTERRUPTS
-#include <Encoder.h>
-
 class Motor
 {
 public:
   Motor(unsigned f_pin,
         unsigned b_pin,
-        unsigned e_pin1, // polarity of these does not matter as far as I can tell
-        unsigned e_pin2,
         uint8_t min_speed) : f_pin(f_pin),
-                             b_pin(b_pin),
-                             encoder(e_pin1, e_pin2),
-                             min_speed(min_speed)
+                             b_pin(b_pin)
   {
     pinMode(f_pin, OUTPUT);
     pinMode(b_pin, OUTPUT);
@@ -24,28 +17,10 @@ public:
   void activity(void);
   void stop(void);
 
-  /* cw (clockwise) and ccw (counterclockwise):
-   * angle (degrees)
-   * - clockwise when looking at the motor along the shaft
-   * - if stop() is called, the Motor will reset its target
-   */
-  void cw(double angle, uint8_t speed);
-  void ccw(double angle, uint8_t speed);
-
-  void move(double angle, uint8_t speed);
   void cw_at_speed(uint8_t speed);
   void ccw_at_speed(uint8_t speed);
 
-  int get_pos(void);
-
-private:
+protected:
   unsigned f_pin,
-      b_pin;
-
-  Encoder encoder;
-
-  int pos;
-  double target;
-  double overflow;
-  uint8_t min_speed;
+           b_pin;
 };
